5d1d3f985abff6ce1113e83db128cffeeb8ed64b
ikehata <ikehata@uts-corp.co.jp>
2015-06-24 14:36:36 +0900

    spec6：コマンドライン引数ID指定

--- recipe.c
+++ recipe.c
@@ -8,6 +8,11 @@ main(int argc, char *argv[]) {
   char line[LINE_BUFFER_SIZE];
   char *file_name = argv[1];
   int    id;
+  int		output_id = 0;
+
+  if (argc == 3) {
+    output_id = atoi(argv[2]);
+  }
 
   file = fopen(file_name, "r");
   if (file == NULL) {
@@ -15,9 +20,15 @@ main(int argc, char *argv[]) {
     return 1;
   }
 
-  for (id = 1; fgets(line, LINE_BUFFER_SIZE, file) != NULL; id++ ) {
-    printf("%d:%s", id, line);
-  }
+	for (id = 1; fgets(line, LINE_BUFFER_SIZE, file); id++) {
+		if (output_id == 0) {
+			printf("%d:%s", id, line);
+		} else {
+			if (id == output_id) {
+				printf("%d:%s", id, line);
+			}
+		}
+	}
 
   return 0;
 }
