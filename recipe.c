5eec201a43a6170b5f26965b6dd2f7867e0d5b42
ikehata <ikehata@uts-corp.co.jp>
2015-06-24 14:48:25 +0900

    spec6：IDの付与、インデント調整

--- recipe.c
+++ recipe.c
@@ -20,15 +20,15 @@ main(int argc, char *argv[]) {
     output_id = atoi(argv[2]);
   }
 
-	for (id = 1; fgets(line, LINE_BUFFER_SIZE, file); id++) {
-		if (output_id == 0) {
-			printf("%d:%s", id, line);
-		} else {
-			if (id == output_id) {
-				printf("%d:%s", id, line);
-			}
-		}
-	}
+  for (id = 1; fgets(line, LINE_BUFFER_SIZE, file); id++) {
+    if (output_id == 0) {
+      printf("%d:%s", id, line);
+    } else {
+      if (id == output_id) {
+        printf("%d:%s", id, line);
+      }
+    }
+  }
 
   return 0;
 }
